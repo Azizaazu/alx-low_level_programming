73c7decf68a404555115f3cce6b5b915684c24e9
Azizaazu <aynaou.aziza98@gmail.com>
2023-03-17 15:32:53 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible combinations of single-digit numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int a;
+int b;
+for (a = 0; a < 10; a++)
+{
+for (b = 1 ; b < 10 ; b++)
+{
+if (a < b)
+{
+putchar (a + '0');
+putchar (b + '0');
+if ((a + b) != 17)
+{
+putchar (',');
+putchar (' ');
+}
+}
+}
+}
+putchar ('\n');
+return (0);
+}
