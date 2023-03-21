0450b562faa2959d8ce98df3d65817e6946ee538
Azizaazu <aynaou.aziza98@gmail.com>
2023-03-21 15:16:56 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * print_last_digit - function that prints the last digi.
+ *
+ * @d: function parameter
+ *
+ * Return: Always b
+ */
+
+int print_last_digit(int d)
+{
+int b;
+b = d % 10;
+if (d < 0)
+b = -b;
+_putchar(b + '0');
+return (b);
+}
