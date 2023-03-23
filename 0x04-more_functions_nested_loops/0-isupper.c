5967ac43756753cb9ee929da2e6f947075ed3515
Azizaazu <aynaou.aziza98@gmail.com>
2023-03-23 09:55:20 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - a function that checks for uppercase character.
+ *
+ * @c: parameter
+ *
+ * Return: 1 if c is uppercase
+ * 0 otherwise
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+0. isupper
+Write a function that checks for uppercase character.
+1. isdigits
+Write a function that checks for a digit (0 through 9).
+2. Collaboration is multiplication
+Write a function that multiplies two integers.
