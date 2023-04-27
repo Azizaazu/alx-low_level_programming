91c4019cb6b6213e74045add1ccaf57d7fd367ed
Azizaazu <aynaou.aziza98@gmail.com>
2023-04-27 16:09:49 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,9 +16,14 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%u] %s\n", h->len, h->str);
+		}
+
 		h = h->next;
 		s++;
 	}
