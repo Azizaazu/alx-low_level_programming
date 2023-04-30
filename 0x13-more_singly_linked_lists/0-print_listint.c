09aec916cb5309df48e7bb39bc106c5379ba9f5c
Azizaazu <aynaou.aziza98@gmail.com>
2023-04-30 13:13:28 -0700

    0-print_listintt.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *c = h;
 	size_t nm = 0;
 
-	while (c != NULL)
+	while (h)
 	{
-		printf("%d\n", c->n);
+		printf("%d\n", h->n);
 		nm++;
-		c = c->next;
+		h = h->next;
 	}
+
 	return (nm);
 }
