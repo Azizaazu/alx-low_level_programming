ee664e92c1e8a17def983168fc4cfe5697fe1af1
Azizaazu <aynaou.aziza98@gmail.com>
2023-08-22 09:13:33 -0700

    update

--- 0x18-dynamic_libraries/9-strcpy.c
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -10,13 +10,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-        int l = 0;
+	int l = 0;
 
-        while (*(src + l) != '\0')
-        {
-                *(dest + l) = *(src + l);
-                l++;
-        }
-        *(dest + l) = '\0';
-        return (dest);
+	while (*(src + l) != '\0')
+	{
+		*(dest + l) = *(src + l);
+		l++;
+	}
+	*(dest + l) = '\0';
+	return (dest);
 }
