a60b696d8b9e4399d492ee6e44833b1b8bd4a11e
Azizaazu <aynaou.aziza98@gmail.com>
2023-09-07 12:09:19 -0700

    update

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: size
+ *
+ * Return: pointer to hash table
+ *
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *t;
+
+	if (size == 0)
+		return (NULL);
+
+	t = calloc(1, sizeof(hash_table_t));
+	if (t == NULL)
+		return (NULL);
+
+	t->size = size;
+	t->array = calloc(size, sizeof(hash_node_t *));
+	if (t->array == NULL)
+	{
+		free(t);
+		return (NULL);
+	}
+	return (t);
+}
+
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,13 @@
+0. >>> ht = {}
+Write a function that creates a hash table.
+1. djb2
+Write a hash function implementing the djb2 algorithm.
+.
+.
+.
+5. >>> print(ht)
+Write a function that prints a hash table.
+6. >>> del ht
+Write a function that deletes a hash table.
+7. $ht['Betty'] = 'Cool'
+In PHP, hash tables are ordered. Wait… WAT? How is this even possible?
